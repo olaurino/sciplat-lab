12e842698f1e211248536e3e34325061b6da707a
adam <athornton@lsst.org>
2022-05-10 16:49:36 -0700

    Work around crypto module bug, unpin JL

--- stage4-jup.sh
+++ stage4-jup.sh
@@ -2,6 +2,20 @@
 set -e
 source ${LOADRSPSTACK}
 
+# This is to work around a problem in JL 3.4.0 and can eventually be removed
+crypto_workaround() {
+    owd=$(pwd)
+    current_scipipe=$(ls /opt/lsst/software/stack/conda/current/envs | \
+			  grep '^lsst-scipipe-' | tail -1)
+    cd /opt/lsst/software/stack/conda/current/envs/${current_scipipe}
+    current_python=$(ls lib | grep ^python | tail -1)
+    cd lib/${current_python}/site-packages/jupyterlab/staging
+
+    # Forgive me for what I am about to do
+    sed -i.bak -e 's/^    plugins$/    plugins,\n    resolve: {\n      fallback: {\n        crypto: false,\n      },\n    }/' webpack.config.js
+    cd ${owd}
+}
+
 # Server, notebook, and lab extensions
 svxt="jupyter_firefly_extensions \
       jupyterlab_iframe"
@@ -35,7 +49,8 @@ jupyter labextension disable \
 # Rebuild the world.
 npm cache clean --force
 jupyter lab clean
-jupyter lab build --dev-build=False --minimize=False
+crypto_workaround
+jupyter lab build --dev-build=False
 
 # List installed labextensions and put them into a format we could consume
 #  for installation
