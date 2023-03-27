9ac305b8e5304b4c0a82df582cf492589d39c34e
adam <athornton@lsst.org>
2023-03-27 16:45:36 -0700

    TeXLive 2023 update

--- stage1-rpm.sh
+++ stage1-rpm.sh
@@ -9,12 +9,12 @@ yum repolist
 yum -y upgrade
 rpm -qa --qf "%{NAME}\n" | xargs yum -y reinstall
 # Add some other packages
-#  gettext and fontconfig are needed for the TexLive installation
-#  jq ... file are generally useful utilities
+#  gettext and fontconfig needed for TeXLive and thus PDF export
+#  perl-Digest-MD5 ... file are generally useful utilities
 #  ...and finally enough editors to cover most people's habits
 yum -y install \
-    gettext fontconfig perl-Digest-MD5 \
-    jq unzip ack screen tmux tree file \
+    gettext fontconfig \
+    perl-Digest-MD5 jq unzip ack screen tmux tree file \
     nano vim-enhanced emacs-nox ed
 # Clear build cache
 yum clean all
--- stage2-os.sh
+++ stage2-os.sh
@@ -39,21 +39,24 @@ cd ${BLD}/tex
 mv ${BLD}/texlive.profile .
 FN="install-tl-unx.tar.gz"
 curl -L http://mirror.ctan.org/systems/texlive/tlnet/${FN} -o ${FN}
+CTAN_MIRROR="https://mirror.math.princeton.edu/pub/CTAN"
 tar xvpfz ${FN}
 ./install-tl-*/install-tl --repository \
-    http://ctan.math.illinois.edu/systems/texlive/tlnet \
+    ${CTAN_MIRROR}/systems/texlive/tlnet \
     --profile ${BLD}/tex/texlive.profile
 rm -rf ${BLD}/tex/${FN} ${BLD}/tex/install-tl*
 # More TeX stuff we need for PDF export
-PATH=/usr/local/texlive/2022/bin/x86_64-linux:${PATH}
+PATH=/usr/local/texlive/2023/bin/x86_64-linux:${PATH}
+
 tlmgr install caption lm adjustbox xkeyval collectbox xcolor \
     upquote eurosym ucs fancyvrb zapfding booktabs enumitem ulem palatino \
     mathpazo tcolorbox pgf environ trimspaces etoolbox float rsfs jknapltx \
-    latexmk dvipng beamer parskip fontspec titling tools unicode-math
+    latexmk dvipng beamer parskip fontspec titling tools unicode-math \
+    lwarp oberdiek pdfcol
 # xetex, bizarrely, has to be installed on its own to get the binaries.
 tlmgr install xetex
-ln -s /usr/local/texlive/2022/bin/x86_64-linux/xelatex \
-      /usr/local/texlive/2022/bin/x86_64-linux/bibtex \
+ln -s /usr/local/texlive/2023/bin/x86_64-linux/xelatex \
+      /usr/local/texlive/2023/bin/x86_64-linux/bibtex \
       /usr/bin
 
 # Clear our caches
